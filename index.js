3f8ed54a4dd229a6a39478ff2f33f870cb6f806f
mellisa <123798125+MariannaCross@users.noreply.github.com>
2023-04-18 17:42:31 -0700

    clean up notes and messages

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
-//const rock = smash (the rock wins because it smashes)
-//const paper = blind (paper wins because it blinds)
-//const scissors = slice (scissors win because they slice)
-// I need the computerChoice to pick between three things, and I can't decide if it matters that 3 is a bigger number than 1, where 1 can never beat 3. 1.2.3. is a line, not a circle and it feels wrong.
-//Modular arithmetic
-//modulo operator, array
 
-//rock,paper,scissors;
-//rock='smash',paper='blind',scissors='slice';
-//trying to look through an array
-//const fight= ["smash","blind","slice"];
-  //  console.log(fight.length)
-   //for(i = Math.floor(Math.random()*3); i < fight.length;i++){
-     //   if (fight[0]="smash") {
-       //     console.log("Smash! Rock Smash!")
-        //}
-        //else if (fight[1]="blind") {
-          //  console.log("Blind! Paper Blind!")
-        //}
-       // else if (fight [2]="slice") {
-        //    console.log("Slice! Scissor Slice!")
-       // }
-   //}
+const fight= ["smash","blind","slice"];
+const rock=fight[0];
+const paper=fight[1]
+const scissors=fight[2]
+//It seems redundant now, but I feel like I need to do it this way to keep it straight in my head.
 
-const fightArray= ["rock","scissors","paper"];
-const getComputerChoice= fightArray[Math.floor(Math.random()*fightArray.length)];
+const getComputerChoice= fight[Math.floor(Math.random()*fight.length)];
 console.log(getComputerChoice);
+//I want the computer choice to be a random rock/scissor/paper in the array so the array loops.
 
-
-
-//
-
+playerSelection=fight[Math.floor(Math.random()*fight.length)];
+console.log(playerSelection); 
+//This is a place marker until I put in the prompt
 //prompt()// to get input from user
 
-//function singleRound(playerSelection,computerSelection){
-  //  if playerSelection > computerSelection
-    //"you win! ____ beats _____"
-    //if else: "you lose. ___ beats ____."
-//}
+function singleRound(playerSelection,getComputerChoice){
+   if (playerSelection > getComputerChoice) {
+   result = "good";
+   } else {
+    result = "not good";
+   }
+ 
+   }
+//I am not having good results here.
+
 
 //function game(){
    // singleRound five times }
-
