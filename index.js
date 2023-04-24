6c2550f4e722202da4185f5172c7ca663421f930
mellisa <123798125+MariannaCross@users.noreply.github.com>
2023-04-24 16:42:59 -0700

    issue: rock, rock you win git push

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 
 const fight= ["rock","scissors","paper"];
 
-
 const getComputerChoice= () => fight[Math.floor(Math.random()*fight.length)];
-console.log(getComputerChoice());
-//I want the computer choice to be a random rock/scissor/paper in the array so the array loops.
-// learned about arrow functions
-playerChoice=fight[Math.floor(Math.random()*fight.length)];
-console.log(playerChoice); 
-//This is a place marker until I put in the prompt
-//prompt()// to get input from user
 
-function singleRound(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
+
+
+function singleRound(playerSelection, computerChoice) {
+    if (playerSelection === computerChoice) {
       console.log("It's a tie!");
     } else if (
-      (playerChoice === "rock" && computerChoice === "scissors")||
-      (playerChoice === "scissors" && computerChoice === "paper")||
-      (playerChoice === "paper" && computerChoice === "rock")
+      (playerSelection === "rock" && computerChoice === "scissors")||
+      (playerSelection === "scissors" && computerChoice === "paper")||
+      (playerSelection === "paper" && computerChoice === "rock")
     ) {
       console.log("You win!");
     } else {
       console.log("You lose!");
     }
   }
+  const playerChoice= () => prompt("Brandish Your Weapon");
+  const playerSelection = playerChoice()
 
-  singleRound(playerChoice,getComputerChoice());
+  console.log(`playerSelection: ${playerSelection}`);
+  console.log(`computerChoice: ${getComputerChoice()}`);
+  singleRound(playerSelection,getComputerChoice());
 
 
  
