cffe07c0c516c616a9f5bd9b75c194c747c76163
mellisa <123798125+MariannaCross@users.noreply.github.com>
2023-04-24 16:55:57 -0700

    added console log to the opening of single round function to solve problem of game logic

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const fight= ["rock","scissors","paper"];
 
 const getComputerChoice= () => fight[Math.floor(Math.random()*fight.length)];
 
-
-
 function singleRound(playerSelection, computerChoice) {
+    console.log(`Player Selection: ${playerSelection}`);
+    console.log(`Computer Choice: ${computerChoice}`);
     if (playerSelection === computerChoice) {
       console.log("It's a tie!");
     } else if (
@@ -18,11 +18,10 @@ function singleRound(playerSelection, computerChoice) {
       console.log("You lose!");
     }
   }
+  //player and computer selections need to be consoled first in order to run through the function in the right way.
   const playerChoice= () => prompt("Brandish Your Weapon");
   const playerSelection = playerChoice()
 
-  console.log(`playerSelection: ${playerSelection}`);
-  console.log(`computerChoice: ${getComputerChoice()}`);
   singleRound(playerSelection,getComputerChoice());
 
 
