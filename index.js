b2d094793ab7be82ee4c92198f0d86bb80fb563e
mellisa <123798125+MariannaCross@users.noreply.github.com>
2023-04-25 16:32:49 -0700

    add wins counter inside game function to access match array

--- index.js
+++ index.js
@@ -28,20 +28,30 @@ function singleRound(playerSelection, computerChoice) {
   const playerChoice= () => prompt("Brandish Your Weapon");
   const playerSelection = playerChoice()
 
-// removing singleRound(playerSelection,getComputerChoice());
-//play 5 times as Game function. Resetting the page to reset the promp keeps resetting the match array.
-  function game (){
+
+
+function game (){
     let match = [];
    for (let i = 0; i < 5; i++) {
     let playerSelection = playerChoice();
     let computerChoice = getComputerChoice();
     let result = singleRound(playerSelection,computerChoice);
     match.push(result)
-   }
+    }
    console.log(match);
+   //counting wins in match array
+   //moving this into the game function
+   let wins = match.filter(result => result ==='win').length;
+    if (wins > 3) {
+    console.log("YOU ARE THE ULTIMATE CHAMPION");
+    } else {
+    console.log("AI WILL TAKE YOUR JOB NOW");
+    }
   }
 
 game(); 
+
+
 
 
 
